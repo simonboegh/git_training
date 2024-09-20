b6594a51e9e9deb645f2adc2d0e5d5515a27ce2b
simonboegh <simonbiisgaard@gmail.com>
2024-09-20 10:34:55 +0000

    commit

--- test_package.R
+++ test_package.R
@@ -1,6 +1,2 @@
 library(httr2)
-<<<<<<< HEAD
-# some comment, with extra explanation
-=======
-# Some comment, here is another explanation
->>>>>>> 2eb08ebb6153eb9ddcc7d5da62280548d7ed1473
+# Some comment, with extra explanation
